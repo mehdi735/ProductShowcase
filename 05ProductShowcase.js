27997674c6406a5cd638494a67d3149c925177b8
Mohammad Mehdi <usmakinaroto718@gmail.com>
2026-08-21 22:13:48 +0330

    test

--- 05ProductShowcase.js
+++ 05ProductShowcase.js
@@ -57,8 +57,6 @@ function createImageIntroduction() {
 }
 
 function createElementImage(src, parent) {
-    if (!src.startsWith("http")) {src = pathServer + src.replace(/^\//, '')};
-
     const img = document.createElement("img");
     if (typeof(parent) === "string") {document.getElementById(parent).appendChild(img);}
     else {parent.appendChild(img)}
