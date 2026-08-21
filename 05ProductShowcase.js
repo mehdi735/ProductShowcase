9c23c8758f8728c2e8507dabfee241faf0300ce2
Mohammad Mehdi <usmakinaroto718@gmail.com>
2026-08-21 22:28:13 +0330

    test

--- 05ProductShowcase.js
+++ 05ProductShowcase.js
@@ -57,7 +57,7 @@ function createImageIntroduction() {
 }
 
 function createElementImage(src, parent) {
-    if (!src.startsWith("http")) {src = pathServer + src.replace(/^\//, '')};
+    if (!src.startsWith("http")) {src = "https://productshowcase-lhrz.onrender.com" + src};
 
     const img = document.createElement("img");
     if (typeof(parent) === "string") {document.getElementById(parent).appendChild(img);}
